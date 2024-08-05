dc38680162c8faefd59558402ba55dbcde9c97af
SELVASIBIBV <727722euec169@skcet.ac.in>
2024-08-05 10:28:56 +0530

    a Please enter the commit message for your changes. Lines starting

--- tipcal.js
+++ tipcal.js
@@ -1,6 +1,5 @@
 function calculateTip() {
-    const billAmount = parseFloat(document.getElementById('billAmount').value);
-    const tipPercentage = parseFloat(document.getElementById('tipPercentage').value);
+ 
     
     if (isNaN(billAmount) || isNaN(tipPercentage)) {
         alert("Please enter valid numbers for bill amount and tip percentage.");
